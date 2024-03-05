f66eb244f894e1ff736f13610413769da1016fe2
Tim Nederhoff <timnederhoff@gmail.com>
2024-03-05 19:15:36 +0100

    chore(): based on files in directory and loop over them

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const fs = require('fs');
 
 const chordsPath = 'path/to/my/songs';
-const song = ['Artist - song.txt'][0]
-try {
-  const data = fs.readFileSync(chordsPath + song, 'utf8');
-  const html = [
-    '<!DOCTYPE html><html lang="en">',
-    '<head><title>Song</title>',
-    '<style>span.chord {color: blue;}</style>',
-    '</head>',
-    '<body><code><pre>',
-    highlightChords(data),
-    '</pre></code></body></html>'
-  ]
-  fs.writeFileSync('output.html', html.join('\n'));
-} catch (err) {
-  console.error(err);
+const files = fs.readdirSync(chordsPath);
+const outputPath = 'output/';
+
+if(!fs.existsSync(outputPath)) {
+  fs.mkdirSync(outputPath);
 }
+
+files.filter(file => file.endsWith('.txt')).forEach(file => {
+  try {
+    const data = fs.readFileSync(chordsPath + file, 'utf8');
+    const html = [
+      '<!DOCTYPE html><html lang="en">',
+      '<head><title>Song</title>',
+      '<style>span.chord {color: blue;}</style>',
+      '</head>',
+      '<body><code><pre>',
+      highlightChords(data),
+      '</pre></code></body></html>'
+    ]
+    fs.writeFileSync('output/' + file.replace('.txt', '.html') , html.join('\n'));
+  } catch (err) {
+    console.error(err);
+  }
+
+})
 
 function highlightChords(input) {
   const chords = ['C', 'D', 'E', 'F', 'G', 'A', 'B'].reduce((previousValue, currentValue) => {
